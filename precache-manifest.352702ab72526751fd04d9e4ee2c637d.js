aa246eb5d141bf0da66b11d57e7471bef77bc6a4
Aravindh-SNR <aravindhsnr@gmail.com>
2020-03-21 00:58:00 +0530

    uibuild

---
+++ precache-manifest.352702ab72526751fd04d9e4ee2c637d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63368cb02196975c145b502e59e53878",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88bcb8b99eb584a2c172",
+    "url": "/static/css/main.0efe3b7d.chunk.css"
+  },
+  {
+    "revision": "5dfdef0a83e89ca6cd65",
+    "url": "/static/js/2.230301dd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.230301dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88bcb8b99eb584a2c172",
+    "url": "/static/js/main.6f21ba84.chunk.js"
+  },
+  {
+    "revision": "ad9c1ae9b2cdeb3f7618",
+    "url": "/static/js/runtime-main.584b4e6a.js"
+  }
+]);
